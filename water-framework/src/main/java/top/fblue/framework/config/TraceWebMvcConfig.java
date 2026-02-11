025fb20a5715cdfb4f7f268236b1ff8d16234cb0
yixing <zhangyixing@powerlaw.ai>
2026-02-11 14:43:07 +0800

    feat 修改 TraceWebMvcConfig

--- water-framework/src/main/java/top/fblue/framework/config/TraceWebMvcConfig.java
+++ water-framework/src/main/java/top/fblue/framework/config/TraceWebMvcConfig.java
@@ -1,27 +1,27 @@
 package top.fblue.framework.config;
 
-import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import top.fblue.framework.interceptor.TraceInterceptor;
 
 /**
  * Web MVC配置类
- * 配置拦截器链：Token认证 → 权限验证
+ * 配置 Trace 拦截器链
  */
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class TraceWebMvcConfig implements WebMvcConfigurer {
 
+    private final HandlerInterceptor traceInterceptor;
 
-    @Resource
-    private TraceInterceptor traceInterceptor;
+    public TraceWebMvcConfig(HandlerInterceptor traceInterceptor) {
+        this.traceInterceptor = traceInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 设置 TraceContext
         registry.addInterceptor(traceInterceptor)
                 .addPathPatterns("/api/**")
-                .order(1);
+                .order(0);
     }
 }
